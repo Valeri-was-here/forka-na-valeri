ed8eeaaf293df3e0a7f2298da36136b0fa3f31a0
Valeri-was-here <valeri.shtinarov@gikn.eu>
2024-06-01 13:55:31 +0300

    ide is glitched :(((((((

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,18 @@
-// Продължи с твоя vanilla JavaScript код тук.
+//
+// document.addEventListener("DOMContentLoaded",() => {
+//     fetch('/db/file.json')
+//         .then(response => response.json())
+//         .then(data => {
+//             console.log(calories);
+//             const calories = document.getElementById('calories');
+//             const nutrition = `<p><b>калории:</b> ${data['calories']}</p>
+//                                         <p><b>калории:</b> ${data['totalFat']}</p>
+//                                         <p><b>калории:</b> ${data['saturatedFat']}</p>
+//                                         <p><b>калории:</b> ${data['totalCarbs']}</p>
+//                                         <p><b>калории:</b> ${data['protein']}</p>
+//                                         <p><b>калории:</b> ${data['sugars']}</p>
+//                                         <p><b>калории:</b> ${data['sodium']}</p>
+//                                         <p><b>калории:</b> ${data['cholesterol']}</p>`;
+//
+//         })
+// })
